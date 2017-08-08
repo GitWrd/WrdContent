61b22d74b0fcd79cc5ed567d718442d5115680a3
GitWrd <17492707+GitWrd@users.noreply.github.com>
2017-08-08 11:23:29 +0200

    Changed to bare Content Provider

--- app/src/main/java/com/wrdijkstra/wrdcontent/activities/MainActivity.java
+++ app/src/main/java/com/wrdijkstra/wrdcontent/activities/MainActivity.java
@@ -25,36 +25,5 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ContentResolver resolver = getContentResolver();
-
-        counter.put(WrdContentContract.Counters._ID, 15 );
-        counter.put(WrdContentContract.Counters.LABEL, "allo?" );
-        counter.put(WrdContentContract.Counters.LOCKED, "false" );
-        counter.put(WrdContentContract.Counters._COUNT, 21 );
-        resolver.insert(WrdContentContract.Counters.CONTENT_URI, counter);
-
-        counter.put(WrdContentContract.Counters._ID, 13 );
-        counter.put(WrdContentContract.Counters.LABEL, "huh?" );
-        counter.put(WrdContentContract.Counters.LOCKED, "true" );
-        counter.put(WrdContentContract.Counters._COUNT, 97 );
-        resolver.insert(WrdContentContract.Counters.CONTENT_URI, counter);
-
-        counterUpdate.put(WrdContentContract.Counters._ID, 13 );
-        counterUpdate.put(WrdContentContract.Counters._COUNT, 117 );
-        resolver.update(WrdContentContract.Counters.CONTENT_URI, counterUpdate, WrdContentContract.Counters._ID + "=?", new String[]{counterUpdate.getAsString(WrdContentContract.Counters._ID)});
-
-        Cursor cursor  = resolver.query(WrdContentContract.Counters.CONTENT_URI, null, null, null, WrdContentContract.Counters.SORT_ORDER_DEFAULT );
-        if (cursor.moveToFirst()){
-            do{
-                int id = cursor.getInt(cursor.getColumnIndex(WrdContentContract.Counters._ID));
-                String label = cursor.getString(cursor.getColumnIndex(WrdContentContract.Counters.LABEL));
-                String locked = cursor.getString(cursor.getColumnIndex(WrdContentContract.Counters.LOCKED));
-                int count = cursor.getInt(cursor.getColumnIndex(WrdContentContract.Counters._COUNT));
-
-                Log.d( "QUERY", "id = " + Integer.toString(id) + ", label = " + label + ", locked = " + locked + ", count = " + Integer.toString(count));
-            }while(cursor.moveToNext());
-        }
-        cursor.close();
     }
 }
